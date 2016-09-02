fef2276e9bd9d4de2379a13a7f1d3a14f8c6d2ab
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2016-09-02 16:31:45 +0100

    Minor tweaks to the vignette

--- R/enrich.glm.R
+++ R/enrich.glm.R
@@ -388,7 +388,7 @@
 
 
 
-#' Function to extract model coefficients from objects of class \code{\link{enriched_glm}}
+#' Function to extract model coefficients from objects of class \code{enriched_glm}
 #'
 #' @param object an object of class \code{\link{enriched_glm}}
 #' @param model either "mean" for the estimates of the parameters in the linear predictor, or "dispersion" for the estimate of the dispersion, or "full" for all estimates
