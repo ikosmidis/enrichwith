a06db519c2b3ef319f2c8b50f1dcd196801aa3dc
Ioannis Kosmidis <ioannis.kosmidis@warwick.ac.uk>
2026-03-18 16:51:13 +0000

    Added P/Q tests

---
+++ tests/testthat/test-PQ.R
@@ -0,0 +1,145 @@
+context("Implementation of P/Q matrices")
+
+library("numDeriv")
+library("MASS")
+
+## A Gamma example, from McCullagh & Nelder (1989, pp. 300-2)
+clotting <- data.frame(
+    u = c(5,10,15,20,30,40,60,80,100, 5,10,15,20,30,40,60,80,100),
+    conc = c(118,58,42,35,27,25,21,19,18,69,35,26,21,18,16,13,12,12),
+    lot = factor(c(rep(1, 9), rep(2, 9))))
+
+test_that("bias implementation matches manual implementation through P and Q [Gamma]", {
+    mod1 <- glm(conc ~ lot*log(u), data = clotting, family = Gamma)
+    mod1e <- enrich(mod1, with = "auxiliary functions")
+    v <- solve(mod1e$auxiliary_functions$information())
+    P <- mod1e$auxiliary_functions$Pmat()
+    Q <- mod1e$auxiliary_functions$Qmat()
+    b0 <- mod1e$auxiliary_functions$bias()
+    expect_equal(b0, - drop(v %*% sapply(seq.int(length(coef(mod1)) + 1), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+
+    coefs <- coef(mod1e)
+    coefs <- coefs * 10
+    disp <- 0.5
+    v <- solve(mod1e$auxiliary_functions$information(coefs, disp))
+    P <- mod1e$auxiliary_functions$Pmat(coefs, disp)
+    Q <- mod1e$auxiliary_functions$Qmat(coefs, disp)
+    b0 <- mod1e$auxiliary_functions$bias(coefs, disp)
+    expect_equal(b0, - drop(v %*% sapply(seq.int(length(coef(mod1)) + 1), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+
+})
+
+## A binomial examples
+data("lizards", package = "brglm2")
+
+test_that("bias implementation matches manual implementation through P and Q [binomial(cauchit)]", {
+    lizardsML <- glm(cbind(grahami, opalinus) ~ height + diameter +
+                     light + time, family = binomial(cauchit), data = lizards)
+    mod1e <- enrich(lizardsML, with = "auxiliary functions")
+    v <- solve(mod1e$auxiliary_functions$information())
+    P <- mod1e$auxiliary_functions$Pmat()
+    Q <- mod1e$auxiliary_functions$Qmat()
+    b0 <- mod1e$auxiliary_functions$bias()
+    expect_equal(b0, - drop(v %*% sapply(seq.int(length(coef(mod1e))), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+    coefs <- coef(mod1e)
+    coefs <- coefs * 10
+    disp <- 0.5
+    v <- solve(mod1e$auxiliary_functions$information(coefs, disp))
+    P <- mod1e$auxiliary_functions$Pmat(coefs, disp)
+    Q <- mod1e$auxiliary_functions$Qmat(coefs, disp)
+    b0 <- mod1e$auxiliary_functions$bias(coefs, disp)
+    expect_equal(b0, - drop(v %*% sapply(seq.int(length(coef(mod1e))), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+})
+
+test_that("bias implementation matches manual implementation through P and Q [binomial(logit)]", {
+    lizardsML <- glm(cbind(grahami, opalinus) ~ height + diameter +
+                         light + time, family = binomial(logit), data = lizards)
+    mod1e <- enrich(lizardsML, with = "auxiliary functions")
+    coefs <- coef(mod1e)
+    nvars <- length(coefs)
+    cnams <- names(coefs)
+    v <- solve(mod1e$auxiliary_functions$information())
+    P <- mod1e$auxiliary_functions$Pmat()
+    Q <- mod1e$auxiliary_functions$Qmat()
+    b0 <- mod1e$auxiliary_functions$bias()
+    expect_equal(b0, - drop(v %*% sapply(seq.int(nvars), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+    coefs <- coefs * 10
+    disp <- 0.5
+    v <- solve(mod1e$auxiliary_functions$information(coefs, disp))
+    P <- mod1e$auxiliary_functions$Pmat(coefs, disp)
+    Q <- mod1e$auxiliary_functions$Qmat(coefs, disp)
+    b0 <- mod1e$auxiliary_functions$bias(coefs, disp)
+    expect_equal(b0, - drop(v %*% sapply(seq.int(nvars), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+    for (k in seq.int(nvars))
+        expect_equal(Q[[k]], matrix(0, nvars, nvars, dimnames = list(cnams, cnams)))
+})
+
+
+
+## A Poisson example
+## Dobson (1990) Page 93: Randomized Controlled Trial :
+counts <- c(18,17,15,20,10,20,25,13,12)
+outcome <- gl(3,1,9)
+treatment <- gl(3,3)
+
+test_that("bias implementation matches manual implementation through P and Q [poisson(log)]", {
+    mod2 <- glm(counts ~ outcome + treatment, family = poisson())
+    enriched_mod2 <- enrich(mod2, with = "auxiliary functions")
+    mod2e <- enrich(mod2, with = "auxiliary functions")
+    coefs <- coef(mod2e)
+    nvars <- length(coefs)
+    cnams <- names(coefs)
+    v <- solve(mod2e$auxiliary_functions$information())
+    P <- mod2e$auxiliary_functions$Pmat()
+    Q <- mod2e$auxiliary_functions$Qmat()
+    b0 <- mod2e$auxiliary_functions$bias()
+    expect_equal(b0, - drop(v %*% sapply(seq.int(nvars), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+    for (k in seq.int(nvars))
+        expect_equal(Q[[k]], matrix(0, nvars, nvars, dimnames = list(cnams, cnams)))
+})
+
+test_that("bias implementation matches manual implementation through P and Q [poisson(sqrt)]", {
+    mod2 <- glm(counts ~ outcome + treatment, family = poisson("sqrt"))
+    enriched_mod2 <- enrich(mod2, with = "auxiliary functions")
+    mod2e <- enrich(mod2, with = "auxiliary functions")
+    coefs <- coef(mod2e)
+    nvars <- length(coefs)
+    cnams <- names(coefs)
+    v <- solve(mod2e$auxiliary_functions$information())
+    P <- mod2e$auxiliary_functions$Pmat()
+    Q <- mod2e$auxiliary_functions$Qmat()
+    b0 <- mod2e$auxiliary_functions$bias()
+    expect_equal(b0, - drop(v %*% sapply(seq.int(nvars), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+})
+
+
+data("coalition", package = "brglm2")
+
+## TODO: Check aliased coefficients - NEEDS FIX in code
+test_that("bias implementation matches manual implementation through P and Q [Gamma - coalition]", {
+    mod1 <- glm(duration ~ fract + I(2 * fract) + numst2, family = Gamma, data = coalition)
+    mod1e <- enrich(mod1, with = "auxiliary functions")
+    v <- solve(mod1e$auxiliary_functions$information())
+    P <- mod1e$auxiliary_functions$Pmat()
+    Q <- mod1e$auxiliary_functions$Qmat()
+    b0 <- mod1e$auxiliary_functions$bias()
+    expect_equal(b0, - drop(v %*% sapply(seq.int(length(coef(mod1)) + 1), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+    coefs <- coef(mod1e)
+    coefs <- coefs * 10
+    disp <- 0.5
+    v <- solve(mod1e$auxiliary_functions$information(coefs, disp))
+    P <- mod1e$auxiliary_functions$Pmat(coefs, disp)
+    Q <- mod1e$auxiliary_functions$Qmat(coefs, disp)
+    b0 <- mod1e$auxiliary_functions$bias(coefs, disp)
+    expect_equal(b0, - drop(v %*% sapply(seq.int(length(coef(mod1)) + 1), function(t) sum(v * (P[[t]] + Q[[t]])) / 2)),
+                 check.attributes = FALSE)
+})
